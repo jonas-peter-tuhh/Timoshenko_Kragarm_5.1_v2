8356f1d9330f44d731823a2c2cba89e086744c5d
jonas <jonas.peter@tuhh.de>
2022-11-23 12:01:28 +0100

    Bis Länge 20

--- Timoshenko_Kragarm_5_1_v2.py
+++ Timoshenko_Kragarm_5_1_v2.py
@@ -308,7 +308,7 @@ plt.grid()
 
 gamma_anal = (((5/Lb)**5 * 0.5 * x**2 - Q0[-1])/(K*A*G))
 gamma_net = v_out_x-phi_out
-gamma_err = np.linalg.norm(gamma_net-gamma_anal, 2)
+gamma_err = np.linalg.norm((gamma_net-gamma_anal), 2)/np.linalg.norm((gamma_anal, 2)[-1])
 print('\u03B3 5.1 =',gamma_err)
 
 
@@ -358,9 +358,7 @@ plt.subplot(2, 2, 1)
 plt.title('$v_{ges}$ Auslenkung')
 plt.xlabel('')
 plt.ylabel('[cm]')
-#v = v
 plt.plot(x, v_out)
-#plt.plot(x, (-1/24 *x**4-np.sin(x)+(Q0[-1]-1)/6 *x**3 - M0[-1]/2 * x**2 +x)/EI - (0.5*x**2 - np.sin(x) - (Q0[-1]-1)*x)/(K*A*G))
 plt.plot(x, (-1/120 *(5/Lb)**5  *(5*x/Lb)**5 + 1/6 * 12.5 * (5*x/Lb)**3 - 41.67/2 * (5*x/Lb)**2)/EI + (1/6 * (5*x/Lb)**3 - 12.5*(5*x/Lb))/(K*A*G))
 plt.grid()
 
@@ -368,9 +366,7 @@ plt.subplot(2, 2, 3)
 plt.title('$\phi$ Neigung')
 plt.xlabel('')
 plt.ylabel('$10^{-2}$')
-#phi = v' - gamma
 plt.plot(x, (v_out_x + gamma_out_x))
-#plt.plot(x, (-1/6 *x**3-np.cos(x)+(Q0[-1]-1)/2 * x**2 - M0[-1]*x+1)/EI)
 plt.plot(x, (-1/24 *(5/Lb)**5 *x**4 + 0.5 * Q0[-1] * x**2 - M0[-1] * x)/EI)
 plt.grid()
 
@@ -378,9 +374,7 @@ plt.subplot(2, 2, 4)
 plt.title('$\kappa$ Krümmung')
 plt.xlabel('Meter')
 plt.ylabel('$(10^{-4})$[1/cm]')
-#kappa = v'' - gamma'
 plt.plot(x, (v_out_xx - gamma_out_x))
-#plt.plot(x, (-0.5*x**2+np.sin(x)+(Q0[-1]-1)*x-M0[-1])/EI)
 plt.plot(x, ( - 1/6 *(5/Lb)**5* x**3 + Q0[-1]*x - M0[-1])/EI)
 plt.grid()
 
@@ -391,12 +385,11 @@ plt.ylabel('$(10^{-2})$')
 #gamma = gamma
 plt.plot(x, (gamma_out))
 plt.plot(x, (((5/Lb)**5 * 0.5 * x**2 - Q0[-1])/(K*A*G)))
-#plt.plot(x, (x-np.cos(x)-(Q0[-1]-1))/(K*A*G))
 plt.grid()
 
 gamma_anal = (((5/Lb)**5 * 0.5 * x**2 - Q0[-1])/(K*A*G))
 gamma_net = gamma_out
-gamma_err = np.linalg.norm(gamma_net-gamma_anal, 2)
+gamma_err = np.linalg.norm((gamma_net-gamma_anal), 2)/np.linalg.norm((gamma_anal, 2)[-1])
 print('\u03B3 5.2 =',gamma_err)
 
 plt.show()
